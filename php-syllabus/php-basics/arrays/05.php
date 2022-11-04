49a8b85d17b9ee794f8e433025858d66f2d470a1
Aleksandrs Butajevs <113251847+Aleksandrs001@users.noreply.github.com>
2022-11-04 10:09:48 +0200

    still not working

--- php-syllabus/php-basics/arrays/05.php
+++ php-syllabus/php-basics/arrays/05.php
@@ -1,142 +1,88 @@
 <?php
-
-$data = [
-    ["row" => 0, "column" => 0], ["row" => 1, "column" => 0], ["row" => 2, "column" => 0],
-    ["row" => 0, "column" => 1], ["row" => 1, "column" => 1], ["row" => 2, "column" => 1],
-    ["row" => 0, "column" => 2], ["row" => 1, "column" => 2], ["row" => 2, "column" => 2]
+$board= [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "],
 ];
 
-$b = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+$combinations=[
+    // Horizontal
+    [[0,0],[0,1],[0,2]],
+    [[1,0],[1,1],[1,2]],
+    [[2,0],[2,1],[2,2]],
+    //Vertical
+    [[0,0],[1,0],[0,2]],
+    [[1,0],[1,1],[2,1]],
+    [[0,2],[1,2],[2,2]],
+    //Diogonal
+    [[0,0],[1,1],[2,2]],
+    [[0,2],[1,1],[2,0]],
 
-function display_board($b)
+];
+
+function displayBoard(array$board)
 {
-
-    echo " $b[0] | $b[1] | $b[2] \n ";
+    echo " {$board[0][0]} | {$board[0][1]} | {$board[0][2]} \n";
     echo "---+---+---\n";
-    echo " $b[3] | $b[4] | $b[5] \n";
+    echo " {$board[1][0]} | {$board[1][1]} | {$board[1][2]} \n";
     echo "---+---+---\n";
-    echo " $b[6] | $b[7] | $b[8] \n";
-
+    echo " {$board[2][0]} | {$board[2][1]} | {$board[2][2]} \n";
 }
 
-$playerOne = "X";
-$playerTwo = "O";
-$turn = $playerOne;
-display_board($b);
+echo "Lets the game begin!\n";
 
-for ($i = 0; $i < 9; $i++)
-{
-    do {
-        $inputOne = (int)readline("$turn Turn| Enter Row: ");
-        $inputTwo = (int)readline("$turn Turn| Enter Column: ");
-        $input = [array("row" => $inputOne, "column" => $inputTwo)];
-        $x = "end";
+displayBoard($board);
 
-        if ($input[0] == $data[0]) {
-            if ($b[0] == "0" || $b[0] == "X") {
-                $x = "repeat";
-                echo "Wrong Input\n";
-            } else $b[0] = $turn;
-        } elseif ($input[0] == $data[1]) {
-            if ($b[1] == "0" || $b[1] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[1] = $turn;
-        } elseif ($input[0] == $data[2]) {
-            if ($b[2] == "0" || $b[2] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[2] = $turn;
-        } elseif ($input[0] == $data[3]) {
-            if ($b[3] == "0" || $b[3] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[3] = $turn;
-        } elseif ($input[0] == $data[4]) {
-            if ($b[4] == "0" || $b[4] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[4] = $turn;
-        } elseif ($input[0] == $data[5]) {
-            if ($b[5] == "0" || $b[5] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[5] = $turn;
-        } elseif ($input[0] == $data[6]) {
-            if ($b[6] == "0" || $b[6] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[6] = $turn;
-        } elseif ($input[0] == $data[7]) {
-            if ($b[7] == "0" || $b[7] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[7] = $turn;
-        } elseif ($input[0] == $data[8]) {
-            if ($b[8] == "0" || $b[8] == "X") {
-                echo "Wrong Input\n";
-                $x = "repeat";
-            } else $b[8] = $turn;
-        } else {
-            $x = "repeat";
-            echo "Wrong Input\n";
+$playerA="X";
+$playerB="O";
+
+$currentPlayer= $playerA;
+
+$turns = 0;
+
+
+while(true) {
+    $turns++;
+
+    $userChoice = readline("\n {$currentPlayer}, choose your location (Row, column) (include )");
+    [$x, $y]= explode(" ", $userChoice);
+
+    if ($x == null || $y == null){
+        displayBoard($board);
+        echo "invalid input";
+        continue;
+    }
+
+    if ($board[$x][$y]==" "){
+        $board[$x][$y]==$currentPlayer;
+    }else{
+        displayBoard($board);
+        echo "Cell is already filled\n";
+        continue;
+    }
+    echo PHP_EOL;
+    displayBoard($board);
+
+    foreach ($combinations as $combination){
+        $conditionCounter=0;
+
+        foreach ($combination as $position){
+            [$x,$y]= $position;
+            if($board[$x][$y] !==$currentPlayer){
+                break;
+            }
+            $conditionCounter++;
         }
 
-        echo "\n";
-
-        display_board($b);
-
-    } while ($x == "repeat");
-
-    $win = "";
-
-    if ($b[0] == "X" && $b[1] == "X" && $b[2] == "X") {
-        $win = $turn;
-    } elseif ($b[0] == "O" && $b[1] == "O" && $b[2] == "O") {
-        $win = $turn;
+        if($conditionCounter==3){
+            echo "Player {$currentPlayer} wins!\n";
+        }
     }
-    if ($b[3] == "X" && $b[4] == "X" && $b[5] == "X") {
-        $win = $turn;
-    } elseif ($b[3] == "O" && $b[4] == "O" && $b[5] == "O") {
-        $win = $turn;
-    }
-    if ($b[6] == "X" && $b[7] == "X" && $b[8] == "X") {
-        $win = $turn;
-    } elseif ($b[6] == "O" && $b[7] == "O" && $b[8] == "O") {
-        $win = $turn;
-    }
-    if ($b[0] == "X" && $b[3] == "X" && $b[6] == "X") {
-        $win = $turn;
-    } elseif ($b[0] == "O" && $b[3] == "O" && $b[6] == "O") {
-        $win = $turn;
-    }
-    if ($b[1] == "X" && $b[4] == "X" && $b[7] == "X") {
-        $win = $turn;
-    } elseif ($b[1] == "O" && $b[4] == "O" && $b[7] == "O") {
-        $win = $turn;
-    }
-    if ($b[2] == "X" && $b[5] == "X" && $b[8] == "X") {
-        $win = $turn;
-    } elseif ($b[2] == "O" && $b[5] == "O" && $b[8] == "O") {
-        $win = $turn;
-    }
-    if ($b[0] == "X" && $b[4] == "X" && $b[8] == "X") {
-        $win = $turn;
-    } elseif ($b[0] == "O" && $b[4] == "O" && $b[8] == "O") {
-        $win = $turn;
-    }
-    if ($b[2] == "X" && $b[4] == "X" && $b[6] == "X") {
-        $win = $turn;
-    } elseif ($b[2] == "O" && $b[4] == "O" && $b[6] == "O") {
-        $win = $turn;
-    }
-    if ($win == $turn) {
-        echo "Player $turn win!\n";
-        exit;
-    }
-
-    $turn = ($turn == $playerOne) ? $playerTwo : $playerOne;
-    if ($i == 8) {
-        echo "Tie!\n";
-    }
+    $isTie=false;
+    foreach ($board as $element)
+        if($turns === 9){
+            echo "Tie!";
+            exit;
+        }
+    $currentPlayer= $currentPlayer=== $playerA? $playerB: $playerA;
 }

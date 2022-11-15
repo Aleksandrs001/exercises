3a5b04063b393814319c24b65cc1c3b0978afa43
Aleksandrs Butajevs <113251847+Aleksandrs001@users.noreply.github.com>
2022-11-15 22:10:21 +0200

    new

---
+++ new/app/CompanyProcessor.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App;
+use League\Csv\Reader;
+use League\Csv\Statement;
+
+
+class CompanyProcessor
+{
+    public Reader $csv;
+    public $limiter;
+
+    public function __construct($csv)
+{
+    $this->csv=$csv;
+}
+
+    public function setLine():Reader
+    {
+
+     $this->csv->setDelimiter(";");
+     $this->csv->setHeaderOffset(0);
+     $this->csv->getHeader();
+     $this->csv->getRecords();
+
+     return $this->csv;
+    }
+
+    public function setLimit(int $min, int $max)
+    {
+        $this->limiter = Statement::create()->offset($min)->limit($max);
+    }
+
+    public function getRecords()
+    {
+        $this->csv->getRecords();
+        return $this->limiter->process($this->csv);
+    }
+
+
+}
+

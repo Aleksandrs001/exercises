304f615625a958a5ce71a866759039558c29b932
Aleksandrs Butajevs <113251847+Aleksandrs001@users.noreply.github.com>
2022-11-22 23:28:42 +0200

    Update ApiProcessor.php

--- new/WeatherWeb/app/ApiProcessor.php
+++ new/WeatherWeb/app/ApiProcessor.php
@@ -14,8 +14,7 @@ class ApiProcessor
     public function __construct($cityName)
     {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $apiKey = $dotenv->load()['SECRET_KEY'];
-        $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={$cityName}&appid={$apiKey}&units=metric";
+        $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={$cityName}&appid={$dotenv->load()['SECRET_KEY']}&units=metric";
         $this->weatherData = json_decode(file_get_contents($apiUrl, true));
         return $this->weatherData;
     }

a7ae1377f3e8f196a08c22da6f6ce1cf97574054
Aleksandrs Butajevs <113251847+Aleksandrs001@users.noreply.github.com>
2022-11-22 19:37:32 +0200

    Add files via upload

---
+++ new/WeatherWeb/app/Weather.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+
+namespace App;
+
+
+class Weather
+{
+    public \stdClass $weatherData;
+
+    public function __construct(\stdClass $weatherData)
+    {
+        $this->weatherData = $weatherData;
+
+        return $this->weatherData;
+    }
+
+
+    public function getName(): string
+    {
+        return $this->weatherData->name;
+    }
+
+
+    public function getTemp(): float
+    {
+        return $this->weatherData->main->temp;
+    }
+
+
+    public function getMaxTemp(): float
+    {
+        return $this->weatherData->main->temp_max;
+    }
+
+
+    public function getMinTemp(): float
+    {
+        return $this->weatherData->main->temp_min;
+    }
+
+
+    public function getPressure(): int
+    {
+        return $this->weatherData->main->pressure;
+    }
+
+
+    public function getHumidity(): int
+    {
+        return $this->weatherData->main->humidity;
+    }
+
+    public function getIcon(): string
+    {
+        return $this->weatherData->weather[0]->icon;
+    }
+
+}
+

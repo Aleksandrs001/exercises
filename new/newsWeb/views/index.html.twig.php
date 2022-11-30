985059df729d5e46578744832e3f63c8991415fb
Aleksandrs Butajevs <113251847+Aleksandrs001@users.noreply.github.com>
2022-11-30 10:15:35 +0200

    Update index.html.twig.php

--- new/newsWeb/views/index.html.twig.php
+++ new/newsWeb/views/index.html.twig.php
@@ -68,7 +68,7 @@
                 <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
                     <article class="overflow-hidden rounded-lg shadow-lg">
                         <a href="{{ article[i].getPicture() }}" target="_blank" >
-                            <img alt="Placeholder" class="block h-auto w-full" src="{{ article[i+1].getPicture() }}">
+                            <img alt="Placeholder" class="block h-auto w-full" src="{{ article[i].getPicture() }}">
                         </a>
                         <header class="flex items-center justify-between leading-tight p-2 md:p-4">
                             <h1 class="text-lg">
